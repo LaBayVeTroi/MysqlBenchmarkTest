41b940ce1f211777b64170cc576eac86ea071960
labayvetroi <namnguyenuetk62@gmail.com>
2020-08-06 10:29:19 +0700

    fix: create records

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,32 +2,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class Main extends Thread{
-    public static String[] updateParams = new String[3];
-    public static String[] updateQuery = new String[]{"INSERT INTO customers (first_name,last_name,email) VALUES ('","','","','","')"};
-    public static void main(String[] args){
-        for (int i = 0; i < 100; i++) {
-            Main main = new Main();
+public class Main extends Thread {
+    public static String[] updateQuery = new String[]{"INSERT INTO customers (first_name,last_name,email) VALUES ('", "','", "','", "')"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            Main main = new Main("debezium_create" + i);
             main.start();
+
         }
     }
 
-    public static Connection createConnection(){
+    public static Connection createConnection() {
         Connection con = null;
-        try{
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/inventory","root","debezium");
-        }catch(Exception e){ System.out.println(e);}
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/demo?useSSL=false", "admin", "pass");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
         return con;
     }
 
     public static void closeConnection(Connection connection) {
-        if (connection != null ) {
+        if (connection != null) {
             try {
                 connection.close();
-            }
-            catch (Exception e ) {
+            } catch (Exception e) {
                 System.out.println(e.toString());
             }
         }
@@ -36,21 +38,32 @@ public class Main extends Thread{
     @Override
     public void run() {
         Connection connection = createConnection();
-        if(connection == null ) {
+        if (connection == null) {
             System.out.println("Failed to create connection !");
             return;
         }
-        try (Statement statement = connection.createStatement();){
-            for (int i = 0; i < 10; i++) {
-                updateParams[0] = "FirstName"+ i + currentThread().getName();
-                updateParams[1] = "LastName" + i + currentThread().getName();
-                updateParams[2] = "email@" + i + currentThread().getName();
+        String[] updateParams = new String[3];
+        try (Statement statement = connection.createStatement();) {
+            for (int i = 0; i < 1000; i++) {
+                updateParams[0] = "FirstName" + currentThread().getName() + "," + i;
+                updateParams[1] = "LastName" + currentThread().getName() + "," + i;
+                updateParams[2] = "Email@" + currentThread().getName() + "," + i;
                 String query = updateQuery[0] + updateParams[0] + updateQuery[1] + updateParams[1] + updateQuery[2] + updateParams[2] + updateQuery[3];
+                System.out.println(query);
                 statement.execute(query);
             }
-        } catch (Exception e ) {
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
         closeConnection(connection);
     }
+
+    public Main(String s) {
+        super(s);
+    }
+
+    public Main() {
+        super();
+    }
 }
+
